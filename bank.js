7e094fc5398f15f7f464dcf9e076865df2675242
Nicola Cortese <nicolacortese92@gmail.com>
2022-04-24 16:03:17 +0100

    added the date arguement to deposit and withdraw, passes all tests

--- bank.js
+++ bank.js
@@ -3,7 +3,7 @@ class Bank {
 
   constructor(){
     this.balance = 1000;
-    
+    this.recorder = [];
   }
 
   
@@ -11,8 +11,22 @@ class Bank {
     return this.balance;
   }
 
-  Deposit (amount) {
+  Deposit (amount,date) {
     this.balance += amount;
+    this.recorder.push(`\n${date} || ${amount} ||  || ${this.balance}`);
+  }
+  
+  Withdraw (amount,date) {
+    this.balance -= amount;
+    this.recorder.push(`\n${date} ||  || ${amount} || ${this.balance}`);
+  }
+
+  AccountStat () {
+    let statement = "date || credit || debit || balance";
+    this.recorder.reverse().forEach(string => {
+      statement += string;
+    });
+    return statement;
   }
 
 
--- bank.test.js
+++ bank.test.js
@@ -17,9 +17,36 @@ describe("expect deposit", () => {
   });
 })
   
-//   describe("expect withdraw", () => {
-//     it("withdraws money - £500", () => {
-//       Withdraw(500);
-//       expect(Balance()).toEqual(500)
-//     });
-//   })
+describe("expect withdraw", () => {
+  it("withdraws money - £500", () => {
+    bank.Withdraw(500);
+    expect(bank.Balance()).toEqual(500)
+  });
+})
+
+describe("account statement", () => {
+  it("prints out the headers", () => {
+    expect(bank.AccountStat()).toEqual("date || credit || debit || balance")
+  });
+
+  it("prints out a deposit", () => {
+    bank.Deposit(1000,"10/01/2023")
+    expect(bank.AccountStat()).toEqual("date || credit || debit || balance\n10/01/2023 || 1000 ||  || 2000")
+  });
+  
+  it("prints out two deposits and a withdrawal", () => {
+    bank.Deposit(1000,"10/01/2023")
+    bank.Deposit(2000,"13/01/2023")
+    
+    expect(bank.AccountStat()).toEqual("date || credit || debit || balance\n13/01/2023 || 2000 ||  || 4000\n10/01/2023 || 1000 ||  || 2000")
+  });
+
+  it("prints out two deposits and a withdrawal", () => {
+    bank.Deposit(1000,"10/01/2023")
+    bank.Deposit(2000,"13/01/2023")
+    bank.Withdraw(500,"14/01/2023")
+
+    expect(bank.AccountStat()).toEqual("date || credit || debit || balance\n14/01/2023 ||  || 500 || 3500\n13/01/2023 || 2000 ||  || 4000\n10/01/2023 || 1000 ||  || 2000")
+  });
+
+})
